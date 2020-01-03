598d93240e9776be42b66c6aae6d5da259be24b6
Tomasz Wasiak <tomek140194@gmail.com>
2020-01-03 23:25:55 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function toggle() {
+    let toggle = document.getElementById('toggle');
+    let prices_monthly = document.querySelectorAll('.monthly_price');
+    let prices_annuall = document.querySelectorAll('.annuall_price');
+    if (toggle.checked == true) {
+      prices_annuall.forEach(price => price.style.display = "none");
+      prices_monthly.forEach(price => price.style.display = "block");
+    } else {
+      prices_annuall.forEach(price => price.style.display = "block");
+      prices_monthly.forEach(price => price.style.display = "none");
+    }
+  }
