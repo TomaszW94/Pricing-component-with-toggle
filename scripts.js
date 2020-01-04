104057c1fcf8b7dcd169b59df0c9c1e93fd4960f
Tomasz Wasiak <tomek140194@gmail.com>
2020-01-04 10:16:26 +0100

    typo

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function toggle() {
     let toggle = document.getElementById('toggle');
     let prices_monthly = document.querySelectorAll('.monthly_price');
-    let prices_annuall = document.querySelectorAll('.annuall_price');
+    let prices_annuall = document.querySelectorAll('.annually_price');
     if (toggle.checked == true) {
       prices_annuall.forEach(price => price.style.display = "none");
       prices_monthly.forEach(price => price.style.display = "block");
